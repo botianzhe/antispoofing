96799aebfd39fcff2dc38471a62f2fb6c257153d
security <ypp865@163.com>
2024-03-08 18:27:26 +0800

    Update extract_frames.py

--- eval_kit/extract_frames.py
+++ eval_kit/extract_frames.py
@@ -20,19 +20,14 @@ def extractface(frame):
     landms = landms[order]
     # Crop faces
     landmarks = landms.detach().numpy().reshape(5, 2).astype(np.int32)
-    maxx,maxy=np.max(landmarks[:,0]),np.max(landmarks[:,1])
-    minx,miny=np.min(landmarks[:,0]),np.min(landmarks[:,1])
-    # img=frame[minx:maxx,miny:maxy,:]
     img = norm_crop(frame, landmarks, image_size=256)
     return np.array(img)
 
 torch.set_grad_enabled(False)
-path='/mnt/UserData1/peipeng/Data/FaceAntiSpoofingTest/'
-targetpath='/mnt/UserData1/peipeng/Data/FaceAntiSpoofingTest_Face/'
+path='/mnt/UserData1/peipeng/Data/FaceAntiSpoofing/'
 num=0
 for root,dir,names in os.walk(path):
     for name in names:
-        ext = os.path.splitext(name)[1]  # 获取后缀名
         filepath = os.path.join(root, name)  # mp4文件原始地址
         name=filepath.replace(path,'')
         if 'jpg' in filepath:
@@ -40,10 +35,9 @@ for root,dir,names in os.walk(path):
 index=0
 for root,dir,names in os.walk(path):
     for name in names:
-        ext = os.path.splitext(name)[1]  # 获取后缀名
         filepath = os.path.join(root, name)  # mp4文件原始地址
         if 'jpg' in filepath:
-            targetfacepath=filepath.replace("FaceAntiSpoofingTest",'FaceAntiSpoofingTest_Face')
+            targetfacepath=filepath.replace("FaceAntiSpoofing",'FaceAntiSpoofing_Face')
             targetfacedir=targetfacepath.replace(name,"")
             print(targetfacedir)
             if not os.path.exists(targetfacedir):
